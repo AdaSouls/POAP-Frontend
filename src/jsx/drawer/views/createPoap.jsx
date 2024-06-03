1dfbc7e5f70e0e6433a6c6ae2885d41b25832ff5
Matias Falcone <matias.falcone@gmail.com>
2024-06-03 12:09:16 +1000

    useNavigate imported from react-router-dom instead of react

--- src/jsx/drawer/views/createPoap.jsx
+++ src/jsx/drawer/views/createPoap.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDrawer, useDrawerDispatch } from '../../contexts/drawer/drawer.provider';
 import eternlWallet from "../../../images/wallets/eternl.jpg";
-import { useState, useNavigate } from 'react';
+import { useState } from 'react';
 
 export default function CreatePoap() {
 
