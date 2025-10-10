785f12e1fb20bb8120b45c20cbcfe08e3f80b88a
pandablue0809 <bluep0415@gmail.com>
2025-10-10 14:29:18 +0100

    fix: updated menu icon

--- src/jsx/layout/sidebar.jsx
+++ src/jsx/layout/sidebar.jsx
@@ -9,6 +9,8 @@ import walletIconInactive from "../../icons/menu/wallet-inactive.png";
 // import settingsIcon from "../../icons/menu/settings.png";
 import adaSoulsIconActive from "../../icons/menu/ada-souls-active.png";
 import adaSoulsIconInactive from "../../icons/menu/ada-souls-inactive.png";
+import poapIconActive from "../../icons/svg/poap-active.svg";
+import poapIconInactive from "../../icons/svg/poap-inactive.svg";
 import { useDrawer } from "../contexts/drawer/drawer.provider";
 
 const Sidebar = ({ activeMenu }) => {
@@ -16,13 +18,13 @@ const Sidebar = ({ activeMenu }) => {
   
   const menus = [
     { id: 1, href: "/", title: "Home", iconActive: homeIcon, iconInactive: homeIcon },
-    { id: 2, href: "/search", title: "Search", iconActive: searchIcon, iconInactive: searchIcon },
-    // { id: 7, href: "/create-event", title: "Create Event", iconActive: soulsIconActive, iconInactive: soulsIconInactive },
-    { id: 3, href: "/souls", title: "Create", iconActive: soulsIconActive, iconInactive: soulsIconInactive },
-    { id: 4, href: "/soulbounds-claim", title: "Claim", iconActive: adaSoulsIconActive, iconInactive: adaSoulsIconInactive },
-    { id: 8, href: "/claim-mint", title: "Claim-Mint", iconActive: adaSoulsIconActive, iconInactive: adaSoulsIconInactive },
+    // { id: 2, href: "/search", title: "Search", iconActive: searchIcon, iconInactive: searchIcon },
+    // // { id: 7, href: "/create-event", title: "Create Event", iconActive: soulsIconActive, iconInactive: soulsIconInactive },
+    // { id: 3, href: "/souls", title: "Create", iconActive: soulsIconActive, iconInactive: soulsIconInactive },
+    // { id: 4, href: "/soulbounds-claim", title: "Claim", iconActive: adaSoulsIconActive, iconInactive: adaSoulsIconInactive },
+    // { id: 8, href: "/claim-mint", title: "Claim-Mint", iconActive: adaSoulsIconActive, iconInactive: adaSoulsIconInactive },
+    { id: 9, href: "/mvp", title: "POAP", iconActive: poapIconActive, iconInactive: poapIconInactive },
     { id: 5, href: "/wallet", title: "Wallet", iconActive: walletIconActive, iconInactive: walletIconInactive },
-    { id: 9, href: "/mvp", title: "MVP", iconActive: adaSoulsIconActive, iconInactive: adaSoulsIconInactive },
     // { id: 6, href: "/settings-profile", title: "Settings", iconActive: settingsIcon, iconInactive: settingsIcon }
     
   ];
